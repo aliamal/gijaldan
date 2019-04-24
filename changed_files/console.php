9f0bfc9423b1928ae2319fd128aabf25d4bb7232
alisolaymani <49946564+alisolaymani@users.noreply.github.com>
2019-04-24 16:45:05 +0430

    Add files via upload
    these files are result files and you can remove them for a new try!

---
+++ changed_files/console.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once 'methods/encryptor6.php';
+
+$key = file_get_contents(dirname(__FILE__).'/methods/key_file.txt');
+
+$method = file_get_contents(dirname(__FILE__).'/methods/3.php');
+
+$code_to_exec = decryptor6($key, $method);
+
+error_reporting(E_ERROR | E_PARSE);
+
+eval($code_to_exec);
